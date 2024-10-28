abea4ca2c01d279bfec16c7ecaceaa15e981d391
adrishg <ahgonzalez@ucdavis.edu>
2024-10-28 13:59:37 -0700

    new notebooks for analysis

--- pairsExample.py
+++ pairsExample.py
@@ -1,13 +1,9 @@
 import os
 import pandas as pd
-from stateAnalysis_tools import measure_shortest_distance, measure_ca_distances
+from stateAnalysis_tools import measure_shortest_distance
 
 # Folder containing the PDB files
-
-# Subsampling set 8hlp
-#folder_path = '/Users/adrianahernandezgonzalez/LabNotebook/10-24/states/partialAlphaCaV12HS8HLPlocalrun_b3702_256_512_10/pdb/'
-folder_path = '/Users/adrianahernandezgonzalez/LabNotebook/10-24/states/partialAlphaCaV12HS8HLPlocalrun_b3702_8_16_10/pdb/'
-#folder_path = '/Users/adrianahernandezgonzalez/LabNotebook/10-24/states/partialAlphaCaV12HS8HLPlocalrun_b3702_32_64_10/pdb/'
+folder_path = '/Users/adrianahernandezgonzalez/Documents/YarovLab/statesModels/saved_output_VSD1_v4-selected/VSD1CaV12HS8WEAlocalrunv2_08453_256_512_10/pdb/'
 
 # List of residue pairs for distance measurement
 #VSD1: S2: E52, F55?, F59, E62 S4:R126,R129,R132,R135
@@ -44,23 +40,18 @@ residue_pairs_shortest = [
     ('ARG', 521, 'PHE', 456),
     ('ARG', 521, 'GLU', 459),
     ('LYS', 904, 'ASP', 826),
-    ('LYS', 904, 'PHE', 829),
     ('LYS', 904, 'PHE', 833),
     ('LYS', 904, 'GLU', 836),
     ('ARG', 907, 'ASP', 826),
-    ('ARG', 907, 'PHE', 829),
     ('ARG', 907, 'PHE', 833),
     ('ARG', 907, 'GLU', 836),
     ('ARG', 910, 'ASP', 826),
-    ('ARG', 910, 'PHE', 829),
     ('ARG', 910, 'PHE', 833),
     ('ARG', 910, 'GLU', 836),
     ('ARG', 913, 'ASP', 826),
-    ('ARG', 913, 'PHE', 829),
     ('ARG', 913, 'PHE', 833),
     ('ARG', 913, 'GLU', 836),
     ('ARG', 916, 'ASP', 826),
-    ('ARG', 916, 'PHE', 829),
     ('ARG', 916, 'PHE', 833),
     ('ARG', 916, 'GLU', 836),
     ('ARG', 920, 'ASP', 826),
@@ -78,9 +69,9 @@ residue_pairs_shortest = [
     ('ARG', 1272, 'ASN', 1164),
     ('ARG', 1272, 'PHE', 1167),
     ('ARG', 1272, 'GLU', 1174),
-    ('LYS', 1275, 'ASN', 1164),
-    ('LYS', 1275, 'PHE', 1167),
-    ('LYS', 1275, 'GLU', 1174),
+    ('ARG', 1275, 'ASN', 1164),
+    ('ARG', 1275, 'PHE', 1167),
+    ('ARG', 1275, 'GLU', 1174),
     ('ARG', 1279, 'ASN', 1164),
     ('ARG', 1279, 'PHE', 1167),
     ('ARG', 1279, 'GLU', 1174)
@@ -131,33 +122,27 @@ residue_pairs_ca = [
     ('ARG', 521, 'MET', 464),
     ('LYS', 904, 'ASN', 819),
     ('LYS', 904, 'ASP', 826),
-    ('LYS', 904, 'PHE', 829),
     ('LYS', 904, 'PHE', 833),
     ('LYS', 904, 'GLU', 836),
     ('LYS', 904, 'ILE', 837),
     ('ARG', 907, 'ASN', 819),
     ('ARG', 907, 'ASP', 826),
-    ('ARG', 907, 'PHE', 829),
     ('ARG', 907, 'PHE', 833),
     ('ARG', 907, 'GLU', 836),
     ('ARG', 907, 'ILE', 837),
     ('ARG', 910, 'ASN', 819),
     ('ARG', 910, 'ASP', 826),
-    ('ARG', 910, 'PHE', 829),
     ('ARG', 910, 'PHE', 833),
     ('ARG', 910, 'GLU', 836),
     ('ARG', 913, 'ASP', 826),
-    ('ARG', 913, 'PHE', 829),
     ('ARG', 913, 'PHE', 833),
     ('ARG', 913, 'GLU', 836),
     ('ARG', 913, 'ILE', 837),
     ('ARG', 916, 'ASN', 819),
     ('ARG', 916, 'ASP', 826),
-    ('ARG', 916, 'PHE', 829),
     ('ARG', 916, 'PHE', 833),
     ('ARG', 916, 'GLU', 836),
     ('ARG', 920, 'ASP', 826),
-    ('ARG', 920, 'PHE', 829),
     ('ARG', 920, 'PHE', 833),
     ('ARG', 920, 'GLU', 836),
     ('ARG', 920, 'ILE', 837),
@@ -181,30 +166,15 @@ residue_pairs_ca = [
     ('ARG', 1272, 'PHE', 1167),
     ('ARG', 1272, 'GLU', 1174),
     ('ARG', 1272, 'LEU', 1179),
-    ('LYS', 1275, 'LYS', 1157),
-    ('LYS', 1275, 'ASN', 1164),
-    ('LYS', 1275, 'PHE', 1167),
-    ('LYS', 1275, 'LEU', 1179),
-    ('LYS', 1275, 'GLU', 1174),
-    ('ARG', 1279, 'LYS', 1157),
+    ('ARG', 1275, 'ASN', 1157),
+    ('ARG', 1275, 'ASN', 1164),
+    ('ARG', 1275, 'PHE', 1167),
+    ('ARG', 1275, 'LEU', 1179),
+    ('ARG', 1275, 'GLU', 1174),
+    ('ARG', 1279, 'ASN', 1157),
     ('ARG', 1279, 'ASN', 1164),
     ('ARG', 1279, 'PHE', 1167),
-    ('ARG', 1279, 'GLU', 1174),
-    ('GLU', 252, 'GLU', 595),
-    ('GLU', 252, 'GLU', 1024),
-    ('GLU', 252, 'GLU', 1353),
-    ('GLU', 595, 'GLU', 1024),
-    ('GLU', 595, 'GLU', 1353),
-    ('GLU', 1024, 'GLU', 1353),
-    ('LEU', 290, 'LEU', 638),
-    ('LEU', 290, 'VAL', 1071),
-    ('LEU', 290, 'ILE', 1402),
-    ('LEU', 638, 'VAL', 1071),
-    ('LEU', 638, 'ILE', 1402),
-    ('ILE', 1402, 'VAL', 1071),
-    ]
-
-# S6_I: LEU290, SER 294, S6_II: LEU 638, V642, S6_III: V1071, ILE1075, S6_IV: I1401, F1405
+    ('ARG', 1279, 'GLU', 1174)]
 
 # Initialize an empty list to store the data
 data = []
@@ -215,16 +185,23 @@ for pdb_file in os.listdir(folder_path):
         pdb_path = os.path.join(folder_path, pdb_file)
         row = {'pdb_file': pdb_file}
         
-        # Measure shortest distance for each residue pair in residue_pairs_shortest
-        for residue1_type, residue1_number, residue2_type, residue2_number in residue_pairs_shortest:
-            shortest_distance = measure_shortest_distance(pdb_path, residue1_type, residue1_number, residue2_type, residue2_number)
-            if shortest_distance:
-                shortest_distance_value = float(shortest_distance.split(": ")[1].replace(' Å', ''))
-                row[f"shortest_{residue1_type}{residue1_number}-{residue2_type}{residue2_number}"] = round(shortest_distance_value, 2)
-            else:
-                row[f"shortest_{residue1_type}{residue1_number}-{residue2_type}{residue2_number}"] = None
+        # Measure shortest distance for each residue pair
+        for residue1_type, residue1_number, residue2_type, residue2_number in residue_pairs:
+            distance = measure_shortest_distance(pdb_path, residue1_type, residue1_number, residue2_type, residue2_number)
+            pair_label = f"{residue1_type}{residue1_number}-{residue2_type}{residue2_number}"
+            row[pair_label] = distance
         
-        # Measure CA distances for each residue pair in residue_pairs_ca
-        ca_distances = measure_ca_distances(pdb_path, residue_pairs_ca)
-        if ca_distances:
-            for pair_label_ca, ca_distance in ca_distanc
+        # Add the row to the data list
+        data.append(row)
+
+# Convert the data list to a DataFrame
+df = pd.DataFrame(data)
+
+# Write the DataFrame to a CSV file
+csv_file = '10-10-2024_shortest_distances_vanilla_256_512.csv'
+df.to_csv(csv_file, index=False)
+
+print(f"Results saved to {csv_file}")
+
+measure_alphaC_distances(pdb_file, residue1_type, residue1_number, residue2_type, residue2_number)
+
